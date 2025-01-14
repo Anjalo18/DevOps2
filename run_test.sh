4890eafcff3f9740d1fa9f018cb81a2039475ad3
Anjalo18 <angelo.bongiovi@edu.ecole-89.com>
2025-01-14 18:36:36 +0100

    MAJ 18:36

--- run_test.sh
+++ run_test.sh
@@ -3,6 +3,8 @@
 # Switch to the "dev" branch
 git checkout dev
 
+echo "-----------------------------"
+
 # Check Python version to verify its installation
 echo "Running tests, checking Python version..."
 python3 --version
@@ -19,32 +21,35 @@ python3 -c "print('Tets passed !')"
 
 # Indicate that the test was successful
 echo "Test passed."
-git status
+echo "-------------------------------"
+echo "SUCCESS"
+
+#git status
 
 # Check for uncommitted changes and handle them
-if [ -n "$(git status --porcelain)" ]; then
-	echo "Changes detected."
-	git add .
-	git commit -m "Changes added on $(date '+%d/%m/%y %H:%M')"
-else
-	echo "No changes  detected"
-fi
+#if [ -n "$(git status --porcelain)" ]; then
+#	echo "Changes detected."
+#	git add .
+#	git commit -m "Changes added on $(date '+%d/%m/%y %H:%M')"
+#else
+#	echo "No changes  detected"
+#fi
 
 # Switch to the "staging" branch and merge changes from "dev"
-git checkout staging
-git merge dev
+#git checkout staging
+#git merge dev
 
 # Confirm if the push was successful or exit with an error
-echo "Etat du dépôt après merge :"
-git status
+#echo "Etat du dépôt après merge :"
+#git status
 
 # Push the "staging" branch to the remote repository
-git push origin staging
+#git push origin staging
 
 # Confirm if the push was succesful or exit with an error
-if [ $? -eq 0 ]; then
-	echo "Push to the 'staging' branch was succesful."
-else
-	echo "Error : Push failed"
-	exit 1
-fi
+#if [ $? -eq 0 ]; then
+#	echo "Push to the 'staging' branch was succesful."
+#else
+#	echo "Error : Push failed"
+#	exit 1
+#fi
